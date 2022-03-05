a65d498680aff57cf2ad7d450445d8f316577be7
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2022-03-05 19:04:15 +0530

    initial

---
+++ CaratRedUI/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaratRedUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ConfirmationSearchCTRL.Instance.OnSearchClick += new EventHandler(searchClick);
+            ConfirmationSearchCTRL.Instance.OnSettingClick = new EventHandler(settingclick);
+            ConfirmationSearchCTRL.Instance.OnBackClick = new EventHandler(redirecttofirstpage);
+
+            DashboardCTRL.Instance.onForiegnClick = new EventHandler(foreginClick);
+            DashboardCTRL.Instance.onPassportClick = new EventHandler(passportClick);
+            DashboardCTRL.Instance.onLocalIDClick = new EventHandler(localClick);
+
+            UserMain.Instance.OnSettingClick = new EventHandler(settingclick);
+            UserMain.Instance.OnSelectReg = new EventHandler(onselectreg);
+
+            ConfirmationSearchCTRL.Instance.OnScanClick = new EventHandler(onscanclick);
+
+            //GuestCard.Instance.OnScanClick = new EventHandler(onscanclick);
+
+            DashboardCTRL.Instance.BackToMain = new EventHandler(backtomain);
+
+            LocalID.Instance.BackToDashBoard = new EventHandler(backtodashboard);
+            LocalID.Instance.BackToConformationSearch = new EventHandler(backtpconformationsearch);
+
+            ForeginCTRL.Instance.BackToDashBoard1 = new EventHandler(backtodashboard);
+            ForeginCTRL.Instance.BackToConformationSearch = new EventHandler(backtpconformationsearch);
+
+            IndianPassportCTRL.Instance.BackToDashBoard2 = new EventHandler(backtodashboard);
+            IndianPassportCTRL.Instance.BackToConformationSearch = new EventHandler(backtpconformationsearch);
+
+            SettingsForm.Instance.BackToMainBoard = new EventHandler(backtomainboard);
+            //DashboardCTRL.Instance.onLocalIDClick = new EventHandler(localClick);
+        }
+        public static void SetDoubleBuffered(System.Windows.Forms.Control c)
+        {
+            if (System.Windows.Forms.SystemInformation.TerminalServerSession)
+                return;
+            System.Reflection.PropertyInfo aProp = typeof(System.Windows.Forms.Control).GetProperty("DoubleBuffered", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            aProp.SetValue(c, true, null);
+        }
+
+        //SetDoubleBuffered(“TableLaoutPannel_controlName”)
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(UserMain.Instance);
+            RootPannel.Controls.Add(UserMain.Instance);
+
+        }
+
+        private void onselectreg(object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            RootPannel.Controls.Add(ConfirmationSearchCTRL.Instance);
+        }
+        private void searchClick(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            RootPannel.Size = DashboardCTRL.Instance.Size;
+            SetDoubleBuffered(DashboardCTRL.Instance);
+            RootPannel.Controls.Add(DashboardCTRL.Instance);
+        }
+
+        public void onscanclick(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            RootPannel.Size = DashboardCTRL.Instance.Size;
+            SetDoubleBuffered(DashboardCTRL.Instance);
+            DashboardCTRL.Instance.CurrentGuestCard = (sender as GuestCard).GuestCardInfo;
+            RootPannel.Controls.Add(DashboardCTRL.Instance);
+        }
+        public void redirecttofirstpage(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(UserMain.Instance);
+            RootPannel.Controls.Add(UserMain.Instance);
+        }
+        private void foreginClick(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(ForeginCTRL.Instance);
+            RootPannel.Controls.Add(ForeginCTRL.Instance);
+        }
+
+
+        private void passportClick(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(IndianPassportCTRL.Instance);
+            RootPannel.Controls.Add(IndianPassportCTRL.Instance);
+        }
+
+        private void localClick(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(LocalID.Instance);
+            RootPannel.Controls.Add(LocalID.Instance);
+        }
+
+        private void backtomain(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(ConfirmationSearchCTRL.Instance);
+            RootPannel.Controls.Add(ConfirmationSearchCTRL.Instance);
+        }
+        private void backtodashboard(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(DashboardCTRL.Instance);
+            RootPannel.Controls.Add(DashboardCTRL.Instance);
+        }
+        private void RootPannel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void settingclick(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(SettingsForm.Instance);
+            RootPannel.Controls.Add(SettingsForm.Instance);
+        }
+        private void backtomainboard(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(UserMain.Instance);
+            RootPannel.Controls.Add(UserMain.Instance);
+        }
+        private void backtpconformationsearch(Object sender, EventArgs e)
+        {
+            RootPannel.Controls.Clear();
+            SetDoubleBuffered(ConfirmationSearchCTRL.Instance);
+            RootPannel.Controls.Add(ConfirmationSearchCTRL.Instance);
+        }
+    }
+}
---
+++ CaratRedUI/IndianPassportCTRL.cs
@@ -0,0 +1,167 @@
+﻿using CaratRedFi_800RLibrary;
+using CaratRedFi800RLibrary;
+using FiScnUtildN;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CaratRedUI
+{
+    public partial class IndianPassportCTRL : UserControl
+    {
+        ScannerConnection sc;
+        Bitmap bitmap;
+        bool Flag = false;
+        Bitmap picture1;
+        Bitmap picture2;
+
+        private static IndianPassportCTRL _instance;
+        public GuestCardInfo CurrentGuestCard { get; set; }
+
+        public EventHandler BackToDashBoard2 { get; set; }
+        public EventHandler BackToConformationSearch { get; set; }
+
+        public static IndianPassportCTRL Instance => _instance ?? (_instance = new IndianPassportCTRL());
+        public IndianPassportCTRL()
+        {
+            Flag = false;
+            InitializeComponent();
+            sc = new ScannerConnection();
+            sc.axFiScn1.ScanToRawEx += axFiScn1_ScanToRawEx;
+            sc.axFiScn1.CreateControl();
+            // label5.Text = CurrentGuestCard.GuestName;
+        }
+
+        private void IndianPassportCTRL_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IndianPassportCTRL_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Flag = true;
+            pictureBox3.Image = null;
+            sc.ForeignId(Handle.ToInt32());
+            //SplitImage(bitmap);
+
+
+            Form2 form2 = new Form2(bitmap);
+            form2.ShowDialog();
+            pictureBox3.Image = form2.SelectedImage;
+            CurrentGuestCard.Image1 = pictureBox3.Image as Bitmap;
+
+            button9.Visible = false;
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (BackToDashBoard2 != null)
+            {
+                BackToDashBoard2(this, new EventArgs());
+            }
+        }
+
+        private void axFiScn1_ScanToRawEx(object sender, AxFiScnLib._DFiScnEvents_ScanToRawExEvent e)
+        {
+            ConvH2BM Conv = new ConvH2BM();
+            bitmap = Conv.GetBitmapFromRAW(e.resolution, e.imageWidth, e.imageLength, e.bitPerPixel, e.compressionType, e.size, e.raw);
+
+            if (Flag == false)
+            {
+                if (pictureBox3.Image == null)
+                {
+                    pictureBox3.Image = bitmap;
+                }
+                else
+                {
+                    pictureBox3.Image = bitmap;
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Flag = true;
+            pictureBox4.Image = null;
+            sc.ForeignId(Handle.ToInt32());
+            //SplitImage(bitmap);
+
+            Form2 form2 = new Form2(bitmap);
+            form2.ShowDialog();
+            pictureBox4.Image = form2.SelectedImage;
+            CurrentGuestCard.Image2 = pictureBox4.Image as Bitmap;
+            button11.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Flag = true;
+            sc.ForeignId(Handle.ToInt32());
+            //SplitImage(bitmap);
+
+
+            Form2 form2 = new Form2(bitmap);
+            form2.ShowDialog();
+            pictureBox3.Image = form2.SelectedImage;
+            CurrentGuestCard.Image1 = pictureBox3.Image as Bitmap;
+
+            button9.Visible = false;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Flag = true;
+            sc.ForeignId(Handle.ToInt32());
+            //SplitImage(bitmap);
+
+            Form2 form2 = new Form2(bitmap);
+            form2.ShowDialog();
+            pictureBox4.Image = form2.SelectedImage;
+            CurrentGuestCard.Image2 = pictureBox4.Image as Bitmap;
+            button11.Visible = false;
+        }
+
+        private async void button12_Click(object sender, EventArgs e)
+        {
+            ApiService apiService = new ApiService();
+
+            picture1 = (Bitmap)pictureBox3.Image;
+            System.IO.MemoryStream ms1 = new MemoryStream();
+            picture1.Save(ms1, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] byteImage1 = ms1.ToArray();
+            CurrentGuestCard.SigBase64_Img1 = Convert.ToBase64String(byteImage1);
+            //textBox1.Text = SigBase641;
+
+            picture2 = (Bitmap)pictureBox4.Image;
+            System.IO.MemoryStream ms2 = new MemoryStream();
+            picture2.Save(ms2, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] byteImage2 = ms2.ToArray();
+            CurrentGuestCard.SigBase64_Img2 = Convert.ToBase64String(byteImage2);
+            //textBox1.Text = SigBase642;
+
+            String response = await apiService.UploadInfoToFile(CurrentGuestCard);
+
+            MessageBox.Show(response);
+
+            CurrentGuestCard.Image1 = picture1;
+            CurrentGuestCard.Image2 = picture2;
+            CurrentGuestCard.Submitted = true;
+            //CurrentGuestCard.GuestName = **** Guest Name through lable
+            if (BackToConformationSearch != null)
+            {
+                BackToConformationSearch(this, new EventArgs());
+            }
+        }
+
+        private void IndianPassportCTRL_Paint(object sender, PaintEventArgs e)
+        {
+            label5.Text = CurrentGuestCard.GuestName;
+        }
+    }
+}
